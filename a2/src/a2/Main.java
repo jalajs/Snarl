b562f0566edc0605f898949c8ba12ef6739d513c
larsonm <larson.m@husky.neu.edu>
2021-01-26 21:15:53 -0500

    Create Main.java

---
+++ a2/src/a2/Main.java
@@ -0,0 +1,13 @@
+package a2;
+
+public class Main {
+
+    // input: 12 [2, "foo", 4]  { "name" : "SwDev", "payload" : [12, 33], "other" : { "payload" : [ 4, 7 ] } }
+    // output --sum:
+    //     [ { "object" : 12, "total" : 12 },
+    //       { "object" : [2, "foo", 4], "total" : 6 },
+    //       { "object" : { "name" : "SwDev", "payload" : [12, 33], "other" : { "payload" : [4, 7] } }, "total" : 45 } ]
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
